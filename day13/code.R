0a5810ca1444902a08c655df99692d9b492ccba0
eshom <erez@mandor>
2021-01-17 00:06:39 +0200

    Day 13 part 2

--- day13/code.R
+++ day13/code.R
@@ -23,4 +23,61 @@ part1 <- function(filename) {
         prod(earliest_arrival(arrival_matrix(read_input(filename))))
 }
 
+read_ids <- function(filename, text) {
+        if (missing(text)) {
+                id <- scan(filename, character(), skip = 1, sep = ",",
+                           quiet = TRUE)
+        } else {
+                id <- scan(text = text, what = character(), sep = ",",
+                           quiet = TRUE)
+        }
+
+        minute <- seq_along(id) - 1
+        minute <- minute[id != "x"]
+        id <- as.numeric(id[id != "x"])
+        attr(id, "minute") <- minute
+        id
+}
+
+find_timestamp <- function(filename, text) {
+        if (!missing(text)) {
+                ids <- read_ids(text = text)
+        } else {
+                ids <- read_ids(filename)
+        }
+        names(ids) <- attributes(ids)$minute
+        attributes(ids)$minute <- NULL
+        generate_seq <- function(from, by, n) {
+                out <- numeric(n)
+                for (i in seq(n) - 1) {
+                        out[i + 1] <- i * by + from
+                }
+                out
+        }
+        reduce_common <- function(x, a) {
+                x[(x + as.numeric(names(a))) %% a == 0]
+        }
+        common_interval <- function(a, b) {
+                prod(a, b)
+        }
+        common_seq <- function(a, b, to = 10^6) {
+                x <- seq(0 - as.numeric(names(a)), to, a)
+                x[(x + as.numeric(names(b))) %% b == 0]
+        }
+        first <- common_seq(ids[1], ids[2])[1]
+        interval <- common_interval(ids[1], ids[2])
+        for (i in 3:length(ids)) {
+                x <- generate_seq(first, interval, 10^6)
+                tmp <- reduce_common(x, ids[i])
+                first <- tmp[1]
+                interval <- diff(tmp)[1]
+        }
+        first
+}
+
+part2 <- function(filename) {
+        find_timestamp(filename)
+}
+
 part1("input.txt")
+part2("input.txt")
